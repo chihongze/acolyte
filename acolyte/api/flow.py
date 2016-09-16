7fef6f196347ad02f574f496f9e9c7fb6093735d
hongze.chi <hongze.chi@shicaigj.com>
2016-09-16 20:21:07 +0800

    对tornado做一点微小的包装，依据声明可以自动生成接口

--- acolyte/api/flow.py
+++ acolyte/api/flow.py
@@ -1,5 +1,14 @@
-from acolyte.api import APIHandlerBuilder
+handlers = [
 
-GetFlowMetaInfoHandler = APIHandlerBuilder(
-    "FlowService", "get_flow_meta_info", "get").\
-    bind_path_var(1, "flow_meta_name").build()
+    # get flow meta info
+    {
+        "url": "/v1/flow/meta/([a-zA-Z0-9]+)",
+        "http_method": "get",
+        "service": "FlowService",
+        "method": "get_flow_meta_info",
+        "path_variables": [
+            "flow_meta_name",
+        ]
+    }
+
+]
--- acolyte/api/route.py
+++ acolyte/api/route.py
@@ -1,8 +1,41 @@
 """本模块用于声明REST API URL地址到handler的映射
 """
-from acolyte.api.flow import GetFlowMetaInfoHandler
+
+from acolyte.api import (
+    flow,
+)
+from acolyte.api import APIHandlerBuilder
 
 
 URL_MAPPING = [
-    (r"/v1/flow/meta/([a-zA-Z0-9_]+)", GetFlowMetaInfoHandler)
+
 ]
+
+_handler_modules = (flow, )
+
+for handler_module in _handler_modules:
+    handlers = getattr(handler_module, "handlers", [])
+    for handler in handlers:
+        builder = APIHandlerBuilder(
+            service_id=handler["service"],
+            method_name=handler["method"],
+            http_mtd=handler["http_method"]
+        )
+
+        # 绑定path变量
+        path_variables = handler.get("path_variables", [])
+        for idx, path_variable in enumerate(path_variables):
+            if isinstance(path_variable, str):
+                builder.bind_path_var(idx + 1, path_variable)
+            else:
+                builder.bind_path_var(idx + 1, *path_variable)
+
+        # 绑定body变量
+        body_variables = handler.get("body_variables", {})
+        for body_var_name, arg_info in body_variables.items():
+            if isinstance(arg_info, str):
+                builder.bind_body_vars(body_var_name, arg_info)
+            else:
+                builder.bind_path_var(body_var_name, *arg_info)
+
+        URL_MAPPING.append((handler["url"], builder.build()))
